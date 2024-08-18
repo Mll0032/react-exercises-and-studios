893799ef7e79417f5d9eee7b23b18e2907bc6c1c
mll0032 <mllawson18@gmail.com>
2024-08-17 21:25:34 -0500

    Updating Lists with appropriate styles and colors adding book images

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Matthew's Top Books";
+   let book1 = "https://m.media-amazon.com/images/I/51j8bRH5sDL._SY445_SX342_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/81-LvK5eDTL._SY522_.jpg";
+   let book3 = "https://i.thriftbooks.com/api/imagehandler/m/88FBA27FAB91EDE99F55B7AAC359D2AFC85B28D8.jpeg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img className="photo" src={book1} style={{height:150, width: 100}} alt="Fourth Wing: Rebecca Yarros" />
+         <img className="photo" src={book2} style={{height:150, width: 100}} alt="Pandemonium: Warren Fahy" />
+         <img className="photo" src={book3} style={{height:150, width: 100}} alt="Legend of the Dragonrealm: Richard A Knaak"/>
       </div>      
    );
 }
