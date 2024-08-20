f3a1266ca3a3c09dc1f031e81a3575a2b69a88df
mll0032 <mllawson18@gmail.com>
2024-08-19 21:12:32 -0500

    Finishing up with styling

--- part1/studio/src/App.jsx
+++ part1/studio/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <div className="recipePhotoBLock">
+        <div className="recipePhotoBlock">
         <RecipePhoto />
         <div>
           <RecipeDescription />
--- part1/studio/src/components/photo.jsx
+++ part1/studio/src/components/photo.jsx
@@ -2,7 +2,7 @@ import styles from './discription'
 
 function RecipePhoto() {
     return (
-        <img src="https://i0.wp.com/mariellamahal.com/wp-content/uploads/2023/03/featured-image_1.2.3.jpg?resize=1024%2C1536&ssl=1" alt="recipe photo"className={styles.imageUpdates} />
+        <img src="https://i0.wp.com/mariellamahal.com/wp-content/uploads/2023/03/featured-image_1.2.3.jpg?resize=1024%2C1536&ssl=1" alt="recipe photo" className={styles.recipePhotoBlock } />
     )
 }
 
